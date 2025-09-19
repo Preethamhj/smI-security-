7f2f1b5cc30a83435cd99b28e73d887427e57f2a
preetham hj <hjpreetham1@gmail.com>
2025-09-19 12:50:03 +0530

    inital environmental setup is over

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import  { pool } from './models/dbconfig.js';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Simple test route
+app.get('/', async (req, res) => {
+  try {
+   
+    const { rows } = await pool.query('SELECT NOW()');
+    console.log('✅ Server connected to the database');
+    console.log('DB Time:', rows[0].now);
+
+    res.json({ serverTime: rows[0].now });
+  } catch (err) {
+    res.status(500).json({ error: 'DB query failed' });
+  }
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`🚀 Server running on port: ${port}`);
+});
