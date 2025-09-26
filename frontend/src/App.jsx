359bb6c50053f22e7175e6a621bd4ae5193aacaf
preetham hj <hjpreetham1@gmail.com>
2025-09-26 21:07:35 +0530

    auth ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import Login from './pages/auth/login'
+import Register from './pages/auth/register'
+import Landingpage from './pages/auth/landingpage'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 function App() {
   return (
-    <div>
-      <h1>Welcome to the App</h1>
-    </div>
+    <Router>
+      <div>
+     
+        <Routes>
+          <Route path="/" element={<Landingpage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
