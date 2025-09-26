67ffb6dfe6c2ca0054d4b21bc1b2fb30c7888e64
preetham hj <hjpreetham1@gmail.com>
2025-09-26 21:00:12 +0530

    envirnonment is ready

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
+
+
+
+
+
 // Simple test route
 app.get('/', async (req, res) => {
   try {
---
+++ backend/routes/auth.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const { Pool } = require('pg');
+const router = express.Router();
+
+dotenv.config();
+
+
